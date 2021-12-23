2f0609b3ebd59fe45eea833a94298caa08c41400
IgoninMihail <92269370+IgoninMihail@users.noreply.github.com>
2021-12-23 19:40:44 +0200

    Добавьте файлы проекта.

---
+++ Lab_2_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("введите x");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("введите y");
+                double y = double.Parse(Console.ReadLine());
+                if (x >= -1 && x <= 1 && x + y >= -1 && x + y <= 0 && y >= -1) Console.WriteLine("да");
+                else Console.WriteLine("нет");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
